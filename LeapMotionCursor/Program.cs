f8a872ecd1caac08af8979bc06a8c4e27c42e713
Fajar Lubis <fajarsidiqlubis13@gmail.com>
2022-10-17 16:26:41 +0700

    Add project files.

---
+++ LeapMotionCursor/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Leap;
+
+namespace LeapMotionCursor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Controller cntrl = new Controller();
+            LeapListener listener = new LeapListener();
+
+            cntrl.AddListener(listener);
+
+            Console.ReadLine();
+
+            cntrl.RemoveListener(listener);
+            cntrl.Dispose();
+        }
+    }
+}
